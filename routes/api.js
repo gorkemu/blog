e70b49154c85eeaa136cbef11b298c59e1914144
gorkemu <gorkemunal88@gmail.com>
2023-04-26 21:01:04 +0300

    get single post page

--- routes/api.js
+++ routes/api.js
@@ -1,17 +1,10 @@
 const express = require("express");
-const post = require("../models/post");
+const Post = require("../models/post");
 const router = express.Router();
-// const { body, validationResult } = require("express-validator");
 
 router.get("/posts", async (req, res) => {
-  // body("title")
-  //   .isLength({ min: 1 })
-  //   .trim()
-  //   .withMessage("Title must be specified.")
-  //   .escape();
-
   try {
-    const posts = await post.find();
+    const posts = await Post.find();
     res.json(posts);
   } catch (err) {
     res.status(500).json({ message: err.message });
@@ -19,9 +12,10 @@ router.get("/posts", async (req, res) => {
 });
 
 router.post("/posts", async (req, res) => {
-  const newPost = new post({
+  const newPost = new Post({
     title: req.body.title,
     content: req.body.content,
+    date: Date.now(),
   });
 
   try {
@@ -29,6 +23,15 @@ router.post("/posts", async (req, res) => {
     res.status(201).json(savedPost);
   } catch (err) {
     res.status(400).json({ message: err.message });
+  }
+});
+
+router.get("/posts/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    res.json(post);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 });
 
