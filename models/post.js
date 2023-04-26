c82789185407b67b0ffe6891939dcb339843f3fc
gorkemu <gorkemunal88@gmail.com>
2023-04-26 21:00:40 +0300

    add date prop & date_formatted virtual prop

--- models/post.js
+++ models/post.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const PostSchema = new Schema({
   title: {
@@ -10,16 +11,24 @@ const PostSchema = new Schema({
     type: String,
     required: true,
   },
-  //   date: {
-  //     type: Date,
-  //     default: Date.now,
-  //     required: true,
-  //   },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
   //   published: {
   //     type: Boolean,
   //     default: false,
   //     required: true,
   //   },
 });
+
+// Virtual for formatted date
+PostSchema.virtual("date_formatted").get(function () {
+  return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATE_MED);
+});
+
+// JSON virtual true
+PostSchema.set("toJSON", { virtuals: true });
 
 module.exports = mongoose.model("Post", PostSchema);
