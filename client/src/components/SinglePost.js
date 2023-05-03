187005acb6df06b9439546d97869211052529737
gorkemu <gorkemunal88@gmail.com>
2023-05-04 02:36:48 +0300

    get all comments for a specific post

--- client/src/components/SinglePost.js
+++ client/src/components/SinglePost.js
@@ -1,5 +1,3 @@
-// page of a specific post
-
 import React from "react";
 import { useState, useEffect } from "react";
 import HTMLReactParser from "html-react-parser";
@@ -26,12 +24,23 @@ const SinglePost = () => {
       .catch((err) => console.log(err));
   }, []);
 
+  // get all comments for this post
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/posts/${id}/comments`)
+      .then((res) => res.json())
+      .then((data) => {
+        setComments(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="singlepost">
       <section>
         <h1>{post.title}</h1>
         <div className="metafooter">
-          <img className="avatar"></img>
+          <img className="avatar" alt=""></img>
           <span className="author-meta">
             <span className="author-name">{post.author} Gorkem Unal</span>
             <br />
@@ -41,12 +50,32 @@ const SinglePost = () => {
           </span>
         </div>
         <div className="post-container">
-          <img src="https://images.unsplash.com/photo-1555181937-efe4e074a301?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=300"></img>
+          <img
+            src="https://images.unsplash.com/photo-1555181937-efe4e074a301?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=200&q=300"
+            alt=""
+          ></img>
           <div className="post">
             <div className="content">{HTMLReactParser(post.content || "")}</div>
           </div>
         </div>
       </section>
+      {comments && ( // if there are comments
+        <section>
+          <h2>Comments</h2>
+          {comments.map((comment) => (
+            <div className="comment" key={comment._id}>
+              <div className="comment-meta">
+                <span className="comment-author-meta">
+                  <span className="comment-author">{comment.name}</span>
+                  <br />
+                  <span className="comment-date">{comment.date_formatted}</span>
+                </span>
+              </div>
+              <div className="comment-content">{comment.content}</div>
+            </div>
+          ))}
+        </section>
+      )}
       <CommentBox />
     </div>
   );
