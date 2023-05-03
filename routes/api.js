b356e35823b9e0f5f20a9b941ab69f521db40366
gorkemu <gorkemunal88@gmail.com>
2023-05-04 02:36:03 +0300

    get comments for a specific post & post a comment

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 const router = express.Router();
 
+// GET all posts
 router.get("/posts", async (req, res) => {
   try {
     const posts = await Post.find();
@@ -11,11 +13,14 @@ router.get("/posts", async (req, res) => {
   }
 });
 
+// POST a new post
 router.post("/posts", async (req, res) => {
   const newPost = new Post({
     title: req.body.title,
     content: req.body.content,
     date: Date.now(),
+    published: req.body.published,
+    comments: [],
   });
 
   try {
@@ -26,10 +31,42 @@ router.post("/posts", async (req, res) => {
   }
 });
 
+// GET a specific post
 router.get("/posts/:id", async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     res.json(post);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// POST a new comment
+router.post("/posts/:id/comments", async (req, res) => {
+  try {
+    const newComment = new Comment({
+      post: req.params.id,
+      content: req.body.content,
+      name: req.body.name,
+      email: req.body.email,
+      date: req.body.date,
+    });
+
+    const savedComment = await newComment.save();
+    const post = await Post.findById(req.params.id);
+    post.comments.push(savedComment);
+    await post.save();
+    res.status(201).json(savedComment);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// GET all comments for a specific post
+router.get("/posts/:id/comments", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id).populate("comments");
+    res.json(post.comments);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
