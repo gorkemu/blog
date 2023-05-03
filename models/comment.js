11ab96dd0859dfc04d44229dcb5bacc32c03cb18
gorkemu <gorkemunal88@gmail.com>
2023-05-04 02:36:34 +0300

    add virtual property for formatted date

--- models/comment.js
+++ models/comment.js
@@ -1,24 +1,41 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const { DateTime } = require("luxon");
 
 const CommentSchema = new Schema({
-  username: {
+  name: {
     type: String,
     required: true,
   },
+
+  email: {
+    type: String,
+    required: true,
+  },
+
   post: {
     type: Schema.Types.ObjectId,
     ref: "Post",
     required: true,
   },
+
   content: {
     type: String,
     required: true,
   },
+
   date: {
     type: Date,
-    default: Date.now,
+    required: true,
   },
 });
+
+// Virtual for formatted date
+CommentSchema.virtual("date_formatted").get(function () {
+  return DateTime.fromJSDate(this.date).toLocaleString(DateTime.DATE_MED);
+});
+
+// JSON virtual true
+CommentSchema.set("toJSON", { virtuals: true });
 
 module.exports = mongoose.model("Comment", CommentSchema);
